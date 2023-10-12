a14a4e99b62460add941ddddc3a8be772df2fa32
iam1947 <145880695+iam1947@users.noreply.github.com>
2023-10-12 22:42:29 +0300

    Update settingsCOMP.js

--- js-components/settingsCOMP.js
+++ js-components/settingsCOMP.js
@@ -1,1 +1,18 @@
-
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Налаштування</title>
+    <link rel="stylesheet" href="https://iam1947.github.io/jsmanach.github.io/style.css">
+    <link rel="stylesheet" href="https://iam1947.github.io/jsmanach.github.io/settings.css">
+  </head>
+  <body>
+	  
+	<div class='app'>
+	   
+	</div>
+	  
+  </body>
+</html>
